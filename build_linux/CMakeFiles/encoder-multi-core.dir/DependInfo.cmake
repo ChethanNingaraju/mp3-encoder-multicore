394177b193e72013ddfe1c907438edf133d2dbee
cningaraju <cningaraju@nvidia.com>
2019-08-26 14:31:54 +0200

    Checking in cmake build fles for reference

---
+++ build_linux/CMakeFiles/encoder-multi-core.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cningaraju/personal/projects/mp3-encoder-multicore/src/encwav.cpp" "/home/cningaraju/personal/projects/mp3-encoder-multicore/build_linux/CMakeFiles/encoder-multi-core.dir/src/encwav.cpp.o"
+  "/home/cningaraju/personal/projects/mp3-encoder-multicore/src/main.cpp" "/home/cningaraju/personal/projects/mp3-encoder-multicore/build_linux/CMakeFiles/encoder-multi-core.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././src/include"
+  "../external/lame-3.100/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
